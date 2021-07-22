7d3784b362bc400c599bd5e68e7481c17f919320
Evan Liu <liuyievan@gmail.com>
2021-07-21 23:22:57 -0700

    miss nader

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #include <Servo.h>
 
 #define CLAW_PIN PB1
-
 #define WEIGHT_ADJUSTOR_PIN PA_3
+
+#define SERVO_PIN PA_4
+
+#define BUTTON PA5
 
 #define PULLEY_MOTOR_PIN PA_8
 #define PULLEY_MOTOR_FREQ 100
+
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -55,6 +59,12 @@ void adjustPulleyMotor(int value)
   pwm_start(PULLEY_MOTOR_PIN, PULLEY_MOTOR_FREQ, value * 4095 / 1023, RESOLUTION_12B_COMPARE_FORMAT);
 }
 
+void changeState() {
+  printToDisplay("AEFWF");
+  delay(3000);
+}
+
+
 // main
 void setup()
 {
@@ -63,9 +73,13 @@ void setup()
   setupClaw();
   // make sure setupDisplay is last because for some reason you gotta call it after all pinModes are done 
   setupDisplay();
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), changeState, HIGH);
 }
 
 void loop()
 {
   int adjustorValue = analogRead(WEIGHT_ADJUSTOR_PIN);
+  printToDisplay("Adjustor: " + String(adjustorValue));
+  adjustPulleyMotor(adjustorValue);
 }
