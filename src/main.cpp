b241b6ddcf68c108d0a2796fe0ab022055355a2e
Evan Liu <liuyievan@gmail.com>
2021-07-30 11:51:28 -0700

    Updated sonar testing

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,14 @@
 #define PULLEY_MOTOR_PIN PA_8
 #define PULLEY_MOTOR_FREQ 100
 
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Servo claw;
+
+#define ECHO PA5
+#define TRIGG PA4
 
 void printToDisplay(String text)
 {
@@ -65,21 +67,41 @@ void changeState() {
 }
 
 
+void sonarSetup() {
+  pinMode(TRIGG, OUTPUT);
+  pinMode(ECHO, INPUT);
+}
+
+long getSonarDistanceInCm() {
+  long duration, inches, cm;
+
+  digitalWrite(TRIGG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGG, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  cm = duration / 29 / 2;
+
+  return cm;
+}
+
 // main
 void setup()
 {
   setupAdjustor();
   setupPulley();
-  setupClaw();
+  // setupClaw();
   // make sure setupDisplay is last because for some reason you gotta call it after all pinModes are done 
   setupDisplay();
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON), changeState, HIGH);
+  // attachInterrupt(digitalPinToInterrupt(BUTTON), changeState, HIGH);
 }
 
 void loop()
 {
   int adjustorValue = analogRead(WEIGHT_ADJUSTOR_PIN);
   printToDisplay("Adjustor: " + String(adjustorValue));
+  printToDisplay("Sonar: " + String(getSonarDistanceInCm()) + " cm");
   adjustPulleyMotor(adjustorValue);
 }
